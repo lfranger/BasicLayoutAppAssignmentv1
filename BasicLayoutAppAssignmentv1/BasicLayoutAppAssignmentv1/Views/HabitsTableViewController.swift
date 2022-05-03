755321621290f9dafa2f3fae8979868cb5d85374
Felipe Garrido <lfranger2099@gmail.com>
2022-05-03 17:58:16 -0400

    Add Delete feature to application

--- BasicLayoutAppAssignmentv1/BasicLayoutAppAssignmentv1/Views/HabitsTableViewController.swift
+++ BasicLayoutAppAssignmentv1/BasicLayoutAppAssignmentv1/Views/HabitsTableViewController.swift
@@ -51,7 +51,44 @@ class HabitsTableViewController: UITableViewController {
         navigationController?.pushViewController(habitDetailVC, animated: true)
         
     }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        switch editingStyle {
+        case .delete:
+            let habitToDelete = persistence.habits[indexPath.row]
+            let habitIndexToDelete = indexPath.row
+            
+            let deleteAlert = UIAlertController(habitTitle: habitToDelete.title) {
+                self.persistence.delete(habitIndexToDelete)
+                tableView.deleteRows(at: [indexPath], with: .automatic)
+            }
+            
+            self.present(deleteAlert, animated: true)
+            
+        default:
+            break
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        persistence.swapHabits(habitIndex: sourceIndexPath.row, destinationIndex: destinationIndexPath.row)
+    }
 
+}
+
+extension UIAlertController {
+    convenience init(habitTitle: String, comfirmHandler: @escaping () -> Void) {
+        self.init(title: "Delete habit", message: "Are you sure you want to delete \(habitTitle)?", preferredStyle: .actionSheet)
+        
+        let confirmAction = UIAlertAction(title: "Confirm", style: .destructive) { _ in
+            comfirmHandler()
+        }
+        self.addAction(confirmAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        self.addAction(cancelAction)
+    }
 }
 
 extension HabitsTableViewController {
@@ -59,6 +96,8 @@ extension HabitsTableViewController {
         title = "Habitual"
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(pressAddHabit(_:)))
         navigationItem.rightBarButtonItem = addButton
+        
+        navigationItem.leftBarButtonItem = self.editButtonItem
     }
     
     @objc func pressAddHabit(_ sender: UIBarButtonItem) {
